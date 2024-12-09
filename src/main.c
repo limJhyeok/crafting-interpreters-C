a1a767c62b941c994b11a0a7bc52ced972b4c291
limJhyeok <ljh6887@gmail.com>
2024-12-09 16:49:17 +0900

    scanning: parentheses

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
+
+#define MAX_TOKEN_SIZE 100
+
+typedef enum TokenType {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    END_OF_FILE
+} TokenType;
+
+typedef struct Token {
+    TokenType type;
+    char *lexeme;
+    char *literal;
+} Token;
 
 char *read_file_contents(const char *filename);
+
+void scanning(const char *file_contents);
 
 int main(int argc, char *argv[]) {
     // Disable output buffering
@@ -21,11 +38,11 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Logs from your program will appear here!\n");
         
         char *file_contents = read_file_contents(argv[2]);
-
         // Uncomment this block to pass the first stage
         if (strlen(file_contents) > 0) {
-            fprintf(stderr, "Scanner not implemented\n");
-            exit(1);
+            scanning(file_contents);
+            // fprintf(stderr, "Scanner not implemented\n");
+            // exit(1);
         } 
         printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
         
@@ -68,4 +85,56 @@ char *read_file_contents(const char *filename) {
     fclose(file);
 
     return file_contents;
+}
+
+void scanning(const char *file_contents){
+    const char *now = &file_contents[0];
+    size_t file_size = strlen(file_contents);
+    Token tokens[MAX_TOKEN_SIZE];
+    int token_index = 0;
+    for (int i = 0; i < file_size; i++){
+        Token token;
+        switch (*now) {
+            case '(':
+                token.type = LEFT_PAREN;
+                token.lexeme = strdup("(");  // 문자열 복사
+                token.literal = NULL;
+                break;
+            case ')':
+                token.type = RIGHT_PAREN;
+                token.lexeme = strdup(")");  // 문자열 복사
+                token.literal = NULL;
+                break;
+            default:
+                printf("Unsupported token type.\n");
+                continue;
+        }
+        tokens[token_index++] = token; 
+        now++;
+    }
+    // Token token;
+    // token.type = END_OF_FILE;
+    // token.lexeme = strdup("");
+    // token.literal = NULL;
+    // tokens[token_index++] = token; 
+
+
+    for (int i = 0; i < token_index; i++) {
+        const char *type_str;
+        switch (tokens[i].type) {
+            case LEFT_PAREN: type_str = "LEFT_PAREN"; break;
+            case RIGHT_PAREN: type_str = "RIGHT_PAREN"; break;
+            case END_OF_FILE: type_str = "EOF"; break;
+            default: type_str = "UNKNOWN"; break;
+        }
+        printf("%s %s %s\n", 
+            type_str,
+            tokens[i].lexeme,
+            tokens[i].literal == NULL ? "null" : tokens[i].literal);
+    }
+
+    // 할당된 메모리 해제
+    for (int i = 0; i < token_index; i++) {
+        free(tokens[i].lexeme);
+    }
 }
